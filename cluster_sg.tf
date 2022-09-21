9d483f1dc6e904d85e2d0b5b8b5ecef4617d0564
Camilla Scuffi <camilla.scuffi@credera.co.uk>
2022-09-21 13:15:54 +0100

    missing import

--- cluster_sg.tf
+++ cluster_sg.tf
@@ -1,6 +1,6 @@
 resource "aws_security_group" "data_ingress_server" {
   name        = "data_ingress_cluster"
-  description = "Rules necesary for pulling container image, accessing vpc endpoints"
+  description = "Rules necessary for pulling container image, accessing vpc endpoints"
   vpc_id      = data.terraform_remote_state.aws_sdx.outputs.vpc.vpc.id
   tags        = merge(local.common_repo_tags, { Name = "data_ingress_cluster" })
   lifecycle {ignore_changes = [tags]}
