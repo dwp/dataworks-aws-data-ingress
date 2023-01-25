84f3719197adf1dd1930712599abbd46ac8ea8a5
Camilla Scuffi <camilla.scuffi@credera.co.uk>
2023-01-25 16:00:07 +0000

    remove receiver e2e

--- terraform/data-ingress-sft-task/sft_agent_s3_config.tf
+++ terraform/data-ingress-sft-task/sft_agent_s3_config.tf
@@ -18,12 +18,6 @@ resource "aws_s3_bucket_object" "data_ingress_sft_agent_application_config_recei
   kms_key_id = var.config_bucket_kms_key_arn
 }
 
-resource "aws_s3_bucket_object" "data_ingress_sft_agent_application_config_receiver_e2e" {
-  bucket     = var.config_bucket.id
-  key        = "${var.sft_agent_config_s3_prefix}/agent-application-config-receiver-e2e.yml"
-  content    = data.template_file.data_ingress_sft_agent_application_config_tpl_receiver_e2e.rendered
-  kms_key_id = var.config_bucket_kms_key_arn
-}
 
 data "template_file" "data_ingress_sft_agent_config_tpl_sender" {
   template = file("${path.module}/sft_config/agent-config-sender.tpl")
@@ -44,14 +38,6 @@ data "template_file" "data_ingress_sft_agent_application_config_tpl_receiver" {
   vars = {
     destination                = var.receiver_destination
     source_filename            = "(.+)"
-  }
-}
-
-data "template_file" "data_ingress_sft_agent_application_config_tpl_receiver_e2e" {
-  template = file("${path.module}/sft_config/agent-application-config-receiver-e2e.tpl")
-  vars = {
-    destination_e2e     =
-    source_filename     = "prod217.csv"
   }
 }
 
