e04fa8822de8979d22fda89ff015a9fd6f57e1b9
camillascuffi <>
2023-03-07 13:55:14 +0000

    test

--- terraform/data-ingress-enable-disable-alarms/schedules.tf
+++ terraform/data-ingress-enable-disable-alarms/schedules.tf
@@ -1,24 +1,24 @@
 resource "aws_cloudwatch_event_rule" "enable" {
-    name = "enable_no_file_landed_alarm"
-    description = "enabling alarm"
-    schedule_expression = "cron(56 13 7 * ? *)"
+    name = "enable_no_file_landed_rule"
+    description = "enabling rule"
+    schedule_expression = "cron(59 13 7 * ? *)"
 }
 
 resource "aws_cloudwatch_event_target" "schedule_lambda_enable" {
     rule = aws_cloudwatch_event_rule.enable.name
-    target_id = "enable_alarm"
+    target_id = "enable_rule"
     arn = aws_lambda_function.enable_rules_lambda.arn
 }
 
 resource "aws_cloudwatch_event_rule" "disable" {
-    name = "disable_no_file_landed_alarm"
-    description = "disabling alarm"
-    schedule_expression = "cron(3 14 7 * ? *)"
+    name = "disable_no_file_landed_rule"
+    description = "disabling rule"
+    schedule_expression = "cron(7 14 7 * ? *)"
 }
 
 resource "aws_cloudwatch_event_target" "schedule_lambda_disable" {
     rule = aws_cloudwatch_event_rule.disable.name
-    target_id = "disable_alarm"
+    target_id = "disable_rule"
     arn = aws_lambda_function.disable_rules_lambda.arn
 }
 
