5b1d8a283d550586c869eea4b44cae0dd8763ad8
camillascuffi <>
2023-03-08 14:33:15 +0000

    -

--- terraform/data-ingress-check-file-landed/schedules.tf
+++ terraform/data-ingress-check-file-landed/schedules.tf
@@ -1,7 +1,7 @@
 resource "aws_cloudwatch_event_rule" "trigger" {
     name = "trigger_lambda_check_file_landed"
     description = "execute lambda"
-    schedule_expression = "cron(${var.shut_down_time})"
+    schedule_expression = replace("cron(${var.shut_down_time})", "* *", "* ? *")
 }
 
 resource "aws_cloudwatch_event_target" "schedule_lambda_check_file_landed" {
