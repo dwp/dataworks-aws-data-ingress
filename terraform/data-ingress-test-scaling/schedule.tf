698a78a0ce6bff067ad3406572654cf35c3ec21f
Jonathan Melia <jonathan.melia@engineering.digital.dwp.gov.uk>
2023-03-28 14:27:45 +0100

    chore: remove timezone

--- terraform/data-ingress-test-scaling/schedule.tf
+++ terraform/data-ingress-test-scaling/schedule.tf
@@ -4,7 +4,7 @@ resource "aws_autoscaling_schedule" "test_on" {
   desired_capacity       = var.asg_instance_count.test_desired
   max_size               = var.asg_instance_count.test_max
   recurrence             = format("%s %s", formatdate("mm hh DD MM", timeadd(timestamp(), "5m")), " *")
-  time_zone              = var.time_zone
+  /* time_zone              = var.time_zone */
   autoscaling_group_name = var.data_ingress_autoscaling_group_name
 }
 
@@ -14,6 +14,6 @@ resource "aws_autoscaling_schedule" "test_off" {
   desired_capacity       = var.asg_instance_count.off
   max_size               = var.asg_instance_count.off
   recurrence             = format("%s %s", formatdate("mm hh DD MM", timeadd(timestamp(), "18m")), " *")
-  time_zone              = var.time_zone
+  /* time_zone              = var.time_zone */
   autoscaling_group_name = var.data_ingress_autoscaling_group_name
 }
