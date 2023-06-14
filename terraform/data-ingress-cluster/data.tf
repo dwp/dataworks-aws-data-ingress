353317d073ee22b7e3f0636750728702d10459da
Jonathan Melia <jonathan.melia@engineering.digital.dwp.gov.uk>
2023-06-14 10:31:02 +0100

    refactor: Trend/Tanium config

--- terraform/data-ingress-cluster/data.tf
+++ terraform/data-ingress-cluster/data.tf
@@ -1,3 +1,8 @@
 data "aws_ec2_managed_prefix_list" "list" {
   name = "dwp-*-aws-cidrs-*"
 }
+
+data "aws_secretsmanager_secret_version" "terraform_secrets" {
+  provider  = aws.management_dns
+  secret_id = "/concourse/dataworks/terraform"
+}
